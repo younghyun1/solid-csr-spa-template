9efa818522c46d69543beac3d7ff9ba553a64fd8
지영현/BE/GN <z308520@kia-partners.com>
2025-07-10 13:48:28 +0900

    feature: image serving under ./public

--- deploy_to_be.sh
+++ deploy_to_be.sh
@@ -4,7 +4,13 @@ rm -rf ../rust-be-template/fe/*
 find ./dist -type f ! -iname '*.png' ! -iname '*.jpg' -exec zstd --ultra -22 -f -o {}.zst {} \;
 
 cd ./dist
+# Copy zstd'd files
 find . -type f -name '*.zst' | while read file; do
+  mkdir -p "../../rust-be-template/fe/$(dirname "$file")"
+  cp "$file" "../../rust-be-template/fe/$file"
+done
+# Copy original .png and .jpg files as-is
+find . -type f \( -iname '*.png' -o -iname '*.jpg' \) | while read file; do
   mkdir -p "../../rust-be-template/fe/$(dirname "$file")"
   cp "$file" "../../rust-be-template/fe/$file"
 done
