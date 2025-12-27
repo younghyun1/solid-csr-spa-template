ace88104d7716e1c77958ff481b4e069ace33ccf
Ji Younghyun <younghyun1@gmail.com>
2025-12-27 05:28:55 -0700

    tailwind v4 bugs fixed

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -6,7 +6,9 @@ export default function Layout(props: RouteSectionProps): JSX.Element {
   return (
     <>
       <TopBar />
-      <main class="transition-colors duration-90 bg-white text-gray-900 dark:bg-gray-950 dark:text-gray-100 min-h-screen">{props.children}</main>
+      <main class="transition-colors duration-90 bg-white text-gray-900 dark:bg-gray-950 dark:text-gray-100 min-h-screen">
+        {props.children}
+      </main>
     </>
   );
 }
